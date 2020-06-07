bd5a0000ec8ce1c277ec7c7551c64efcbf8483e5
Victor Oliveira <victor.conceicao01@fatec.sp.gov.br>
2020-06-07 20:16:41 -0300

    Adequando projeto

---
+++ web.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const packageInfo = require('./package.json');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({ version: packageInfo.version });
+});
+
+const server = app.listen(process.env.PORT, '0.0.0.0', () => {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(`Web server started at http://${host}:${port}`);
+});
+
+module.exports = bot => {
+    app.post(`/${bot.token}`, (req, res) => {
+        bot.processUpdate(req.body);
+        res.sendStatus(200);
+    });
+};
